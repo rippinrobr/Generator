2a6eee155d36969d78cab4483013102496a34a65
rippinrobr <rippinrobr@hotmail.com>
2011-02-12 16:27:21 -0500

    removeded the win32 directory

--- win32/ui/init.rb
+++
@@ -1,9 +0,0 @@
-require 'PresentationFramework'
-require 'PresentationCore'
-require File.dirname(__FILE__) + '/../../lib/parsers/read_raw_input'
-require File.dirname(__FILE__) + '/main_win'
-require File.dirname(__FILE__) + '/controllers/application_controller' 
-
-cec = ApplicationController.new 'win32/ui/views/class_viewer.xaml'
-@win = WindowsManager.new cec
-@win.show_window
