807ae99d40222f5606df7ee2469af726f933e2f1
rippinrobr <rippinrobr@hotmail.com>
2011-10-13 15:54:49 -0400

    moved the ar connection code into its own file so I can re-use it in the loader code. Cleaned up the migration_generator class to remove duplicated code

--- lib/generator/languages/ruby/migration_generator.rb
+++ lib/generator/languages/ruby/migration_generator.rb
@@ -25,24 +25,29 @@ class MigrationGenerator
   end
 
   def generate_migration
-    template = File.join(File.dirname(__FILE__),
-                         "templates/migration.erb")
-    b = binding
-    engine = ERB.new(File.read(template), 0, '-%>')
-    write_class_file(@options[:migration_output_dir], 
-                     @options[:migration_file_name],
-                     engine.result(b),
-                     "rb")
+    process_template("templates/migration.erb",
+                     @options[:migration_output_dir],
+                     @options[:migration_file_name])
   end
 
   def generate_ar_model
+    process_template("templates/ar_model.erb", 
+                     @options[:migration_model_dir],
+                     "#{@options[:db_table_name]}_source")
+  end
+
+  def generate_ar_connection
+    process_template( "templates/ar_connection.erb", 
+                      @options[:migration_output_dir],
+                      "ar_connection")
+  end
+
+private
+  def process_template(template_path, output_dir, file_name)
     template = File.join(File.dirname(__FILE__),
-                         "templates/ar_model.erb")
+                         template_path)
     b = binding
     engine = ERB.new(File.read(template), 0, '-%>')
-    write_class_file(@options[:migration_model_dir], 
-                     "#{@options[:db_table_name]}_source",
-                     engine.result(b),
-                     "rb")
+    write_class_file(output_dir, file_name, engine.result(b), "rb")
   end
 end
